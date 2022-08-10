3186f681f763a31d838e8a0dc0b64e4c7f8f61a1
Admin <ngoctran260202@gmail.com>
2022-08-10 22:33:26 +0700

    fix menu exception

--- src/main.java
+++ src/main.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
-
+import javax.swing.UIManager;
 public class main {
     public static void main(String[] args) throws IOException {
-        menu m = new menu();
-        m.Menu();
-        m.selection();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            menu m = new menu();
+            m.Menu();
+            m.selection();
+        } catch (Exception e) {
+
+        }
+
     }
 }
